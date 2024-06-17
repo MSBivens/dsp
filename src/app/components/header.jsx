cedfd4a7261501ed7201a8abc622a0e080baf7ef
Michael Bivens <mikebiv95@gmail.com>
2024-06-16 21:42:08 -0700

    header draft

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,129 +1,166 @@
+// import Image from "next/image";
+// import logo from "../../../public/images/logo.png";
+
+"use client";
+
+// import React from "react";
+// import { useState } from "react";
+
+import { useState, useEffect } from "react";
+
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function handleClick() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  useEffect(() => {
+    const collapseMenu = document.getElementById("collapseMenu");
+
+    if (isMenuOpen) {
+      collapseMenu.style.display = "block";
+    } else {
+      collapseMenu.style.display = "none";
+    }
+  }, [isMenuOpen]);
+
   return (
-    <header>
-      <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="https://flowbite.com" class="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              class="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
-            />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
-          </a>
-          <div class="flex items-center lg:order-2">
-            <a
-              href="#"
-              class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            >
-              Donate
-            </a>
-            <a
-              href="#"
-              class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-            >
-              Volunteer
-            </a>
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span class="sr-only">Open main menu</span>
-              <svg
-                class="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <svg
-                class="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div
-            class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-            id="mobile-menu-2"
+    <header class="flex shadow-md py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
+      <div class="flex flex-wrap items-center justify-between gap-5 w-full">
+        <a href="javascript:void(0)">
+          <img
+            src="https://readymadeui.com/readymadeui.svg"
+            alt="logo"
+            class="w-36"
+          />
+        </a>
+
+        <div
+          id="collapseMenu"
+          class="max-lg:hidden lg:!block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50"
+        >
+          <button
+            id="toggleClose"
+            class="lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3"
           >
-            <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Company
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Marketplace
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Features
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Team
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-4 fill-black"
+              viewBox="0 0 320.591 320.591"
+            >
+              <path
+                d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
+                data-original="#000000"
+              ></path>
+              <path
+                d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
+                data-original="#000000"
+              ></path>
+            </svg>
+          </button>
+
+          <ul class="lg:flex gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
+            <li class="mb-6 hidden max-lg:block">
+              <a href="javascript:void(0)">
+                <img
+                  src="https://readymadeui.com/readymadeui.svg"
+                  alt="logo"
+                  class="w-36"
+                />
+              </a>
+            </li>
+            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                class="hover:text-[#007bff] text-[#007bff] block font-semibold text-[15px]"
+              >
+                Home
+              </a>
+            </li>
+            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+              >
+                About Us
+              </a>
+            </li>
+            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+              >
+                Alumni Chapter
+              </a>
+            </li>
+            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+              >
+                Events
+              </a>
+            </li>
+            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+              >
+                News
+              </a>
+            </li>
+            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+              >
+                Gamma Eye
+              </a>
+            </li>
+            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+              >
+                History
+              </a>
+            </li>
+            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+              >
+                Philanthropy
+              </a>
+            </li>
+          </ul>
         </div>
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <p>Test</p>
+
+        <div class="flex max-lg:ml-auto space-x-3">
+          <button class="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
+            Donate
+          </button>
+          <button class="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
+            Volunteer
+          </button>
+
+          <button id="toggleOpen" class="lg:hidden">
+            <svg
+              class="w-7 h-7"
+              fill="#000"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <p>Test</p>
-        </div>
-      </nav>
+      </div>
     </header>
   );
 }
