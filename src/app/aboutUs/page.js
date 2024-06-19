1f28921ba8b1c3c1ab3afcab6f894fbe624cdcd1
Michael Bivens <mikebiv95@gmail.com>
2024-06-18 18:38:49 -0700

    content update

--- src/app/aboutUs/page.js
+++ src/app/aboutUs/page.js
@@ -12,10 +12,10 @@ export default function AboutUs() {
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
               Alumni Corporation Board
             </h2>
-            {/* <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-              Explore the whole collection of open-source web components and
-              elements built with the utility classes from Tailwind
-            </p> */}
+            <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+              Fiscal and operational oversight of the phsycial and monetary
+              assets of Gamma Iota
+            </p>
           </div>
           <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             <div class="text-center text-gray-500 dark:text-gray-400">
@@ -109,10 +109,8 @@ export default function AboutUs() {
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
               Collegiate Chapter Advising Board
             </h2>
-            {/* <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-              Explore the whole collection of open-source web components and
-              elements built with the utility classes from Tailwind
-            </p> */}
+            Advisors to the undergraduate chapter, furthering the mission of
+            personal and professional growth.
           </div>
           <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             <div class="text-center text-gray-500 dark:text-gray-400">
@@ -194,6 +192,53 @@ export default function AboutUs() {
                 Neil Sims
               </h3>
               <p>Vue.js Developer</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Alumni Chapter People */}
+      <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+          <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Alumni Chapter
+            </h2>
+            <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+              Striving for the betterment of the alumni experience for every
+              brother by providing opportunities for networking, support, and
+              social engagement.
+            </p>
+          </div>
+          <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div class="text-center text-gray-500 dark:text-gray-400">
+              <Image
+                class="mx-auto mb-4 w-36 h-36 rounded-full"
+                src={placeholderImage}
+              ></Image>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Vacant
+              </h3>
+              <p>President</p>
+            </div>
+            <div class="text-center text-gray-500 dark:text-gray-400">
+              <Image
+                class="mx-auto mb-4 w-36 h-36 rounded-full"
+                src={placeholderImage}
+              ></Image>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Vacant
+              </h3>
+              <p>Treasurer</p>
+            </div>
+            <div class="text-center text-gray-500 dark:text-gray-400">
+              <Image
+                class="mx-auto mb-4 w-36 h-36 rounded-full"
+                src={placeholderImage}
+              ></Image>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Vacant
+              </h3>
+              <p>Secretary</p>
             </div>
           </div>
         </div>
