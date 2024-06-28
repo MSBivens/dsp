1fcba38c96571beadbe480b4cc16078597b99dbb
Michael Bivens <mikebiv95@gmail.com>
2024-06-27 20:23:16 -0700

    abstracted hero

--- src/app/gammaEye/page.js
+++ src/app/gammaEye/page.js
@@ -1,30 +1,11 @@
 import Contact from "../components/contact.jsx";
+import GammaEyeSubscribe from "../components/gammaEyeSubscribe.jsx";
 
 export default function GammaEye() {
   return (
     <main>
       {/* Sign Up */}
-      <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-          <div class="mx-auto max-w-screen-md sm:text-center">
-            <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">
-              Sign up to receive the Gamma Eye
-            </h2>
-            <p class="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">
-              Stay up to date with Gamma Iota news, events, and more by updating
-              your contact information today!
-            </p>
-            <div class="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
-              <button
-                type="submit"
-                class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Subscribe
-              </button>
-            </div>
-          </div>
-        </div>
-      </section>
+      <GammaEyeSubscribe />
       {/* Most Recent Issue */}
       <section class="bg-gray-100 dark:bg-gray-800">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
