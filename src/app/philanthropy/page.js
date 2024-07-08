8a6bc53cc62ba6d1b15564310e9e2614f717ac4b
Michael Bivens <mikebiv95@gmail.com>
2024-07-08 12:44:11 -0700

    styling update

--- src/app/philanthropy/page.js
+++ src/app/philanthropy/page.js
@@ -5,8 +5,8 @@ import placeholderImage from "../../../public/images/placeholder.png";
 export default function philanthropy() {
   return (
     <main>
-      {/* Hero Section */}
-      <section class="bg-white dark:bg-gray-900">
+      {/* Hero Section - remove? */}
+      {/* <section>
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
@@ -42,164 +42,147 @@ export default function philanthropy() {
             </a>
           </div>
         </div>
-      </section>
-      {/* Feature Section*/}
-      <section class="bg-white py-24 sm:py-32">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto max-w-2xl lg:text-center">
-            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+      </section> */}
+      {/* Feature Section */}
+      <section>
+        <div class="items-center max-w-screen-xl px-4 py-8 mx-auto lg:grid lg:grid-cols-4 lg:gap-16 xl:gap-24 lg:py-24 lg:px-6">
+          <div class="col-span-2 mb-8">
+            <h2 class="mt-3 mb-4 text-3xl font-extrabold tracking-tight text-gray-900 md:text-3xl dark:text-white">
               Supporting Causes: Making a Differeince in Our Community
-            </p>
-            <p class="mt-6 text-lg leading-8 text-gray-600">
+            </h2>
+            <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
               At Gamma Iota, we are proud to have raised a significant amount to
               support various causes and make a positive impact in our
               community. Through our philanthropic efforts, we strive to create
               a abetter future for those in need.
             </p>
           </div>
-          <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-            <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-              <div class="relative pl-16">
-                <dt class="text-base font-semibold leading-7 text-gray-900">
-                  <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <svg
-                      class="h-6 w-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                      />
-                    </svg>
-                  </div>
-                  Impactful Giving
-                </dt>
-                <dd class="mt-2 text-base leading-7 text-gray-600">
-                  Every donation counts. Together, we have raised over $100,000
-                  to support other causes
-                </dd>
-              </div>
-              <div class="relative pl-16">
-                <dt class="text-base font-semibold leading-7 text-gray-900">
-                  <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <svg
-                      class="h-6 w-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                      />
-                    </svg>
-                  </div>
-                  Community Impact
-                </dt>
-                <dd class="mt-2 text-base leading-7 text-gray-600">
-                  Our philanthropic efforts have amde a significant difference
-                  in the lives of many.
-                </dd>
-              </div>
-            </dl>
+          <div class="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
+            <div>
+              <svg
+                class="w-10 h-10 mb-2 text-purple-500 md:w-12 md:h-12 dark:text-purple-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <h3 class="mb-2 text-2xl font-bold dark:text-white">
+                Impactful Giving
+              </h3>
+              <p class="font-light text-gray-500 dark:text-gray-400">
+                Every donation counts. Together, we have raised over $100,000 to
+                support other causes
+              </p>
+            </div>
+            <div>
+              <svg
+                class="w-10 h-10 mb-2 text-purple-500 md:w-12 md:h-12 dark:text-purple-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
+              </svg>
+              <h3 class="mb-2 text-2xl font-bold dark:text-white">
+                Community Impact
+              </h3>
+              <p class="font-light text-gray-500 dark:text-gray-400">
+                Our philanthropic efforts have amde a significant difference in
+                the lives of many.
+              </p>
+            </div>
           </div>
         </div>
       </section>
-      {/* Featured Image */}
-      <section class="bg-white dark:bg-gray-900">
-        <figure class="h-auto max-w-lg mx-auto">
-          <Image
-            className="h-auto rounded-lg"
-            src={placeholderImage}
-            alt="Placeholder Image"
-          ></Image>
-          <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
-            Image caption
-          </figcaption>
-        </figure>
-      </section>
-      {/* Benefits Section */}
-      <section class="bg-white py-24 sm:py-32">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto max-w-2xl lg:text-center">
-            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              The Benefits of Philantropy for Recipients and Brothers
+      {/* B2B Video */}
+      <section>
+        <div class="py-8 px-4 mx-auto max-w-screen-xl ">
+          <div class="flex flex-col justify-center text-center">
+            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              Bike to Boise
+            </h1>
+            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+              Please enjoy our montage of our brothers biking from Moscow to
+              Boise, a tradition over 60 years old!
             </p>
-            <p class="mt-6 text-lg leading-8 text-gray-600">
+          </div>
+          <div>
+            <iframe
+              class="mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl"
+              src="https://www.youtube.com/embed/KaLxCiilHns"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </section>
+      {/* Benefits */}
+      <section>
+        <div class="items-center max-w-screen-xl px-4 py-8 mx-auto lg:grid lg:grid-cols-4 lg:gap-16 xl:gap-24 lg:py-24 lg:px-6">
+          <div class="col-span-2 mb-8">
+            <h2 class="mt-3 mb-4 text-3xl font-extrabold tracking-tight text-gray-900 md:text-3xl dark:text-white">
+              The Benefits of Philantropy for Recipients and Brothers
+            </h2>
+            <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
               Philanthropy has a postiive impact on both the recipienmts and the
               brothers. Through charitbale giving and volunteer work, we are
               able to make a difference in the lives of others while also
               fostering personal growth and a sense of purpose within our
               fraternity.
             </p>
-          </div>
-          <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-            <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-              <div class="relative pl-16">
-                <dt class="text-base font-semibold leading-7 text-gray-900">
-                  <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <svg
-                      class="h-6 w-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                      />
-                    </svg>
-                  </div>
+            <div class="pt-6 mt-6 col-span-2 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
+              <div>
+                <svg
+                  class="w-10 h-10 mb-2 text-purple-500 md:w-12 md:h-12 dark:text-purple-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <h3 class="mb-2 text-2xl font-bold dark:text-white">
                   Impactful Giving
-                </dt>
-                <dd class="mt-2 text-base leading-7 text-gray-600">
+                </h3>
+                <p class="font-light text-gray-500 dark:text-gray-400">
                   Our philanthhropic efforts create lasting change and improve
                   the lives of others in need.
-                </dd>
+                </p>
               </div>
-              <div class="relative pl-16">
-                <dt class="text-base font-semibold leading-7 text-gray-900">
-                  <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <svg
-                      class="h-6 w-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                      />
-                    </svg>
-                  </div>
+              <div>
+                <svg
+                  class="w-10 h-10 mb-2 text-purple-500 md:w-12 md:h-12 dark:text-purple-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
+                </svg>
+                <h3 class="mb-2 text-2xl font-bold dark:text-white">
                   Personal Growth
-                </dt>
-                <dd class="mt-2 text-base leading-7 text-gray-600">
+                </h3>
+                <p class="font-light text-gray-500 dark:text-gray-400">
                   Engaging in philanthropy helps our brothers develop leadership
                   skills and empathy.
-                </dd>
+                </p>
               </div>
-            </dl>
+            </div>
           </div>
-          <Image
-            className="h-auto rounded-lg"
-            src={placeholderImage}
-            alt="Placeholder Image"
-          ></Image>
+          <div class="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+              <Image src={placeholderImage}></Image>
+            </div>
+          </div>
         </div>
       </section>
     </main>
