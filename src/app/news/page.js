283e7ee954663ae63643df4fb7da4d0b81f4f07b
Michael Bivens <mikebiv95@gmail.com>
2024-07-10 18:04:13 -0700

    styling update

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -6,7 +6,7 @@ export default function news() {
   return (
     <main>
       {/* Hero Section */}
-      <section class="bg-white dark:bg-gray-900">
+      <section>
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
           <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
             Latest
