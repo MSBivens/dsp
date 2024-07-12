21080fd3f0f87c36b706736cd2af53ba8a5d4216
Michael Bivens <mikebiv95@gmail.com>
2024-07-11 21:22:19 -0700

    Sanity progress

---
+++ dsp/lib/interface.js
@@ -0,0 +1,27 @@
+// export interface simpleBlogCard {
+//     title: string;
+//     smallDescription: string;
+//     currentSlug: string;
+//     titleImage: any;
+//   }
+
+//   export interface fullBlog {
+//     currentSlug: string;
+//     title: string;
+//     content: any;
+//     titleImage: any;
+//   }
+
+const simpleBlogCard = {
+  title: string,
+  smallDescription: string,
+  currentSlug: string,
+  titleImage: any,
+};
+
+const fullBlog = {
+  currentSlug: string,
+  title: string,
+  content: any,
+  titleImage: any,
+};
--- dsp/src/app/news/page.js
+++ dsp/src/app/news/page.js
@@ -3,33 +3,33 @@ import UpcomingEvents from "../components/upcomingEvents.jsx";
 import placeholderImage from "../../../public/images/placeholder.png";
 
 // Test
-import Link from "next/link";
-export const revalidate = 30; // revalidate at most 30 seconds
-import { Card, CardContent } from "@/components/ui/card";
-import { simpleBlogCard } from "./lib/interface";
-import { client, urlFor } from "./lib/sanity";
+// import Link from "next/link";
+// export const revalidate = 30; // revalidate at most 30 seconds
+// import { Card, CardContent } from "@/components/ui/card";
+// import { client, urlFor } from "./lib/sanity";
 
-async function getData() {
-  const query = `
-  *[_type == 'blog'] | order(_createdAt desc) {
-    title,
-      smallDescription,
-      "currentSlug": slug.current,
-      titleImage
-  }`;
+// import simpleBlogCard from "../../../lib/interface.js";
 
-  const data = await client.fetch(query);
+// async function getData() {
+//   const query = `
+//   *[_type == 'blog'] | order(_createdAt desc) {
+//     title,
+//       smallDescription,
+//       "currentSlug": slug.current,
+//       titleImage
+//   }`;
 
-  return data;
-}
+//   const data = await client.fetch(query);
+
+//   return data;
+// }
 
 // ///////////////
 
-
-export default function news() {
-  // 
-  const data: simpleBlogCard[] = await getData();
-// 
+export default async function news() {
+  //
+  // const data = await getData();
+  //
   return (
     <main>
       {/* Hero Section */}
@@ -46,31 +46,32 @@ export default function news() {
           </p>
         </div>
       </section>
+      {/* Temp Message */}
+      <h1>This Page Is Under Construction</h1>
       {/* Blog Post List */}
-      <div className="grid grid-cols-1  md:grid-cols-2 mt-5 gap-5">
-      {data.map((post, idx) => (
-        <Card key={idx}>
-          <Image
-            src={urlFor(post.titleImage).url()}
-            alt="image"
-            width={500}
-            height={500}
-            className="rounded-t-lg h-[200px] object-cover"
-          />
+      {/* <div className="grid grid-cols-1  md:grid-cols-2 mt-5 gap-5">
+        {data.map((post, idx) => (
+          <Card key={idx}>
+            <Image
+              src={urlFor(post.titleImage).url()}
+              alt="image"
+              width={500}
+              height={500}
+              className="rounded-t-lg h-[200px] object-cover"
+            />
 
-          <CardContent className="mt-5">
-            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
-            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
-              {post.smallDescription}
-            </p>
-            <Button asChild className="w-full mt-7">
-              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
-            </Button>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
-      {/* ////////////////// */}
+            <CardContent className="mt-5">
+              <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+              <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+                {post.smallDescription}
+              </p>
+              <Button asChild className="w-full mt-7">
+                <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
+      </div> */}
     </main>
   );
 }
