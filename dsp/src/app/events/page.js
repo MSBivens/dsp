7a53437740377ac9fe4898c88647303f33d6b062
Michael Bivens <mikebiv95@gmail.com>
2024-07-12 08:51:56 -0700

    deployment fixes

--- dsp/src/app/events/page.js
+++ dsp/src/app/events/page.js
@@ -10,7 +10,7 @@ export default function events() {
         <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
           <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              Don't Miss The Next Event
+              Don&apos;t Miss The Next Event
             </h2>
             <p className="mb-4">
               The events hosted by the undergraduate and alumni chapter are a
